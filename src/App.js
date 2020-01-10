192cc0b6d6645857aed644f3001e08971984669b
beekeshsingh <er.beekeshsingh@gmail.com>
2020-01-10 15:54:49 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Hello CI!
         </h3>
         <p>
-          A demo website for showing different CI pipelines.
+          A demo website for showing different CI pipelines Beekesh Singh.
         </p>
         <a
           className="App-link"
